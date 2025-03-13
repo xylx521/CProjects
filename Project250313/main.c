34c80074b75022f2f3808ad043b99ab03478b6b4
xylx521 <hx384391724@163.com>
2025-03-13 22:04:32 +0800

    更新

--- Project250313/main.c
+++ Project250313/main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    int a; 
-    scanf("%d", &a);
-    printf("你好. a = %d\n", a);
+    int a = 10; 
+    int arr1[10] = {0};
+    printf("arr1[0] = %d\na = %d\n", arr1[0], a);
+    printf("请输入arr1[0]的值: ");
+    scanf("%d", &arr1[0]);
+    printf("arr1[0] = %d\n", arr1[0]);
     return 0;
 }
