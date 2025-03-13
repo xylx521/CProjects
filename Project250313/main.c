01722ebf315ca23bf73d8ba73af65c8a22e461fd
xylx521 <hx384391724@163.com>
2025-03-13 14:49:55 +0800

    第一个测试项目

---
+++ Project250313/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a; 
+    scanf("%d", &a);
+    printf("你好. a = %d\n", a);
+    system("pause");  // Windows 特有的暂停命令
+    return 0;
+}
